efc585c1dc50c017d53b838480d79697f00364c0
tirumalasettiv <venkat.tsetti@gmail.com>
2025-10-20 18:20:41 -0400

    Update: Sync current version of Expense Management App

--- frontend/app.py
+++ frontend/app.py
@@ -7,14 +7,14 @@ from expenses import maintain_expenses
 from analytics import view_analytics
 
 # Load and apply CSS styles
-st.write(os.getcwd())
+'''st.write(os.getcwd())
 css_file_path = os.path.join(os.getcwd(), "style.css")
 if os.path.exists(css_file_path):
     with open(css_file_path) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 else:
     st.error("CSS file not found. Please ensure 'style.css' exists in the current directory.")
-
+'''
 api_url = "http://127.0.0.1:8000"
 
 st.title("Expense Management App")
